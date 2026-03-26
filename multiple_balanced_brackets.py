5a38c53813d0d6681005f5b21053c77ad5ba9fcd
zenthiccc <djesph@gmail.com>
2026-03-27 00:20:24 +0800

    simplify PRINT_IDS

--- multiple_balanced_brackets.py
+++ multiple_balanced_brackets.py
@@ -4,19 +4,19 @@ def is_balanced(s):
     global PRINT_IDS
     state = "q0"
     stack = ["Z"]
-    
-    if PRINT_IDS == True:
+
+    if PRINT_IDS:
         print("Processing " + s)
 
     for i in range(len(s)):
         char = s[i]
         remaining_input = s[i:]
-        
+
         stack_str = ""
         for item in reversed(stack):
             stack_str = stack_str + item
-            
-        if PRINT_IDS == True:
+
+        if PRINT_IDS:
             print("ID: (" + state + ", " + remaining_input + ", " + stack_str + ")")
 
         if state == "q0":
@@ -24,11 +24,11 @@ def is_balanced(s):
                 stack.append("!")
                 state = "q1"
             else:
-                if PRINT_IDS == True:
+                if PRINT_IDS:
                     print("Invalid string. Failed at position " + str(i + 1) + ".")
                     print("Remaining unprocessed input string: " + remaining_input)
                 return False
-                
+
         elif state == "q1":
             if char == "x":
                 pass
@@ -38,7 +38,7 @@ def is_balanced(s):
                 if stack[-1] == "<":
                     stack.pop()
                 else:
-                    if PRINT_IDS == True:
+                    if PRINT_IDS:
                         print("Invalid string. Failed at position " + str(i + 1) + ".")
                         print("Remaining unprocessed input string: " + remaining_input)
                     return False
@@ -46,7 +46,7 @@ def is_balanced(s):
                 if stack[-1] == "{":
                     stack.pop()
                 else:
-                    if PRINT_IDS == True:
+                    if PRINT_IDS:
                         print("Invalid string. Failed at position " + str(i + 1) + ".")
                         print("Remaining unprocessed input string: " + remaining_input)
                     return False
@@ -54,7 +54,7 @@ def is_balanced(s):
                 if stack[-1] == "[":
                     stack.pop()
                 else:
-                    if PRINT_IDS == True:
+                    if PRINT_IDS:
                         print("Invalid string. Failed at position " + str(i + 1) + ".")
                         print("Remaining unprocessed input string: " + remaining_input)
                     return False
@@ -62,27 +62,27 @@ def is_balanced(s):
                 if stack[-1] == "(":
                     stack.pop()
                 else:
-                    if PRINT_IDS == True:
+                    if PRINT_IDS:
                         print("Invalid string. Failed at position " + str(i + 1) + ".")
                         print("Remaining unprocessed input string: " + remaining_input)
                     return False
             elif char == "!":
                 if stack[-1] == "!":
                     stack.pop()
-                    state = "q2" 
+                    state = "q2"
                 else:
-                    if PRINT_IDS == True:
+                    if PRINT_IDS:
                         print("Invalid string. Failed at position " + str(i + 1) + ".")
                         print("Remaining unprocessed input string: " + remaining_input)
                     return False
             else:
-                if PRINT_IDS == True:
+                if PRINT_IDS:
                     print("Invalid string. Failed at position " + str(i + 1) + ".")
                     print("Remaining unprocessed input string: " + remaining_input)
                 return False
-        
+
         elif state == "q2":
-            if PRINT_IDS == True:
+            if PRINT_IDS:
                 print("Invalid string. Failed at position " + str(i + 1) + ".")
                 print("Remaining unprocessed input string: " + remaining_input)
             return False
@@ -90,25 +90,25 @@ def is_balanced(s):
     stack_str = ""
     for item in reversed(stack):
         stack_str = stack_str + item
-        
-    if PRINT_IDS == True:
+
+    if PRINT_IDS:
         print("ID: (" + state + ", E, " + stack_str + ")")
 
     if state == "q2" and len(stack) == 1 and stack[0] == "Z":
-        if PRINT_IDS == True:
+        if PRINT_IDS:
             print("q2 is a final state.")
             print(s + " is valid and has balanced brackets.")
         return True
     else:
-        if PRINT_IDS == True:
+        if PRINT_IDS:
             print("Invalid string. " + state + " is not a final state.")
         return False
 
 
 def evaluate(s):
-    counts = [0] 
+    counts = [0]
     inner_string = s[1:-1]
-    
+
     for char in inner_string:
         if char == "x":
             counts[-1] = counts[-1] + 1
@@ -116,7 +116,7 @@ def evaluate(s):
             counts.append(0)
         elif char == ">":
             val = counts.pop()
-            counts[-1] = counts[-1] + (val * 2) 
+            counts[-1] = counts[-1] + (val * 2)
         elif char == "}":
             val = counts.pop()
             counts[-1] = counts[-1] + (val + 1)
@@ -129,7 +129,7 @@ def evaluate(s):
                 counts[-1] = counts[-1] + (val - 1)
             else:
                 counts[-1] = counts[-1] + 0
-                
+
     return counts[0]
 
 
@@ -137,12 +137,12 @@ def main1():
     file = open("input.txt", "r")
     lines = file.readlines()
     file.close()
-    
+
     for line in lines:
         clean_line = line.strip()
         if clean_line != "":
             is_balanced(clean_line)
-            print("") 
+            print("")
 
 
 def main2():
@@ -152,12 +152,12 @@ def main2():
     file = open("input.txt", "r")
     lines = file.readlines()
     file.close()
-    
+
     for line in lines:
         clean_line = line.strip()
         if clean_line != "":
             is_valid = is_balanced(clean_line)
-            
+
             if is_valid == True:
                 answer = evaluate(clean_line)
                 print(clean_line + " - Resulting number of x's: " + str(answer))
